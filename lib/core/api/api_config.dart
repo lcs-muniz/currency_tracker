f6535bcdc0329b3b0d3d806f1688de4d3c7dd96b
Lucas Muniz <lucas.muniz.ifpr@gmail.com>
2025-09-23 02:54:22 -0300

    feat(dotenv): adicionando dotenv

--- lib/core/api/api_config.dart
+++ lib/core/api/api_config.dart
@@ -1,3 +1,11 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class ApiConfig {
-  static const String baseUrl = 'https://api.exchangerate-api.com/v4/latest/';
+  static final String apiKey = dotenv.env['API_KEY'] ?? 'CHAVE_NAO_ENCONTRADA';
+
+  static String getBaseUrl(String baseCurrency) {
+    print("🔑 Chave de API carregada: $apiKey");
+    print("🌐 Construindo URL para base: $baseCurrency");
+    return 'https://v6.exchangerate-api.com/v6/$apiKey/latest/$baseCurrency';
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,22 @@ import 'package:currency_tracker/core/theme/theme_controller.dart';
 import 'package:currency_tracker/presentation/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:signals_flutter/signals_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'core/theme/app_theme.dart';
 
 Future<void> main() async {
+  try {
+    await dotenv.load(fileName: ".env");
+
+    print("✅ Arquivo .env carregado! Chave da API: ${dotenv.env['API_KEY']}");
+  } catch (e) {
+    print("❌ ERRO AO CARREGAR O ARQUIVO .env: $e");
+  }
+
   setupDependencyInjection();
   final themeController = injector.get<ThemeController>();
-  
+
   runApp(
     Watch(
       (_) => MaterialApp(
